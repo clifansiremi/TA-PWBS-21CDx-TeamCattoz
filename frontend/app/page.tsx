3dbd17f09a6feaaf5692558274fb41a4e47707a8
riskaokta <riska_oktafia@teknokrat.ac.id>
2024-06-08 15:17:32 +0700

    pia:up beranda

---
+++ frontend/app/page.tsx
@@ -0,0 +1,6 @@
+// panggil function ViewMahasiswa
+import ViewMahasiswa from './view/page';
+
+export default function Home() {
+  return <ViewMahasiswa />;
+}
---
+++ frontend/app/view/page.tsx
@@ -0,0 +1,19 @@
+// import module/package "swr"
+import useSWR from 'swr';
+
+// definisi "fetcher"
+const fetcher = (url: string) => fetch(url).then((response) => response.json());
+
+export default function View() {
+  // definisi "swr"
+  const { data } = useSWR('http://localhost:8000/api/view', fetcher);
+  return (
+    <div>
+      <table className="w-full">
+        {/* judul tabel */}
+
+        {/* isi tabel */}
+      </table>
+    </div>
+  );
+}
