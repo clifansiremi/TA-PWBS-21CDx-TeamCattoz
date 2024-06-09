83e57515956ac83e05bce36466401b75194967cd
riskaokta <riska_oktafia@teknokrat.ac.id>
2024-06-09 14:13:33 +0700

    ubah view

--- frontend-admin/app/view/page.tsx
+++ frontend-admin/app/view/page.tsx
@@ -6,7 +6,6 @@ const fetcher = (url: string) => fetch(url).then((response) => response.json());
 
 export default function View() {
   // definisi "swr"
-  const { data } = useSWR('http://localhost:8000/api/view', fetcher);
   return (
     <div>
       <table className="w-full">
