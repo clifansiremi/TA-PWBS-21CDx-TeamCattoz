d7a5ba048d3cf4a28b93fa3b856ee8b0dc5c91b9
riskaokta <riska_oktafia@teknokrat.ac.id>
2024-06-10 21:15:06 +0700

    mengedit konservasi laut

---
+++ frontend-admin/app/ksvlaut/konservasi-laut.tsx
@@ -0,0 +1,13 @@
+import Navbar from '../componen/navbar';
+
+const KonservasiLaut: React.FC = () => {
+  return (
+    <div>
+      <h1>Konservasi Laut</h1>
+      <Navbar />
+      <p>Informasi mengenai konservasi laut.</p>
+    </div>
+  );
+};
+
+export default KonservasiLaut;
