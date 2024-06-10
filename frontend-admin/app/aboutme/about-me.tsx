3204ad0036cd71a14d760fa52f2d4f8766fc3654
riskaokta <riska_oktafia@teknokrat.ac.id>
2024-06-10 21:13:39 +0700

    mengedit about me

---
+++ frontend-admin/app/aboutme/about-me.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+import Navbar from '../componen/navbar';
+
+const AboutMe: React.FC = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <Navbar />
+      <p>Selamat datang di halaman About Me!</p>
+    </div>
+  );
+}
+
+
+export default AboutMe;
