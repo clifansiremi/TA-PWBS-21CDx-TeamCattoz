f57bb1c33c3be38ab41f6f18434ce8ba3453e341
riskaokta <riska_oktafia@teknokrat.ac.id>
2024-06-10 20:51:01 +0700

    membuat isi pengelolaan perikanan

---
+++ frontend-admin/app/pengelolaperikanan/page.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+import Navbar from '../components/Navbar';
+
+const PengelolaanPerikanan: React.FC = () => {
+  return (
+    <div>
+      <h1>Pengelolaan Perikanan</h1>
+      <Navbar />
+      <p>Informasi mengenai pengelolaan perikanan.</p>
+    </div>
+  );
+}
+
+export default PengelolaanPerikanan;
