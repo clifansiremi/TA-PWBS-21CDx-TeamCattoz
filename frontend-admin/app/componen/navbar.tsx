c4ca52cdb524683db920bb86283d14a1f2bb064d
riskaokta <riska_oktafia@teknokrat.ac.id>
2024-06-10 20:50:10 +0700

    membuat komponen navbar untuk menyambungkan ke hal menu lainnya

---
+++ frontend-admin/app/componen/navbar.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Navbar: React.FC = () => {
+  return (
+    <nav>
+      <ul>
+        <li><Link href="/">Beranda</Link></li>
+        <li><Link href="/login">Login</Link></li>
+        <li><Link href="/konservasi-laut">Konservasi Laut</Link></li>
+        <li><Link href="/pengelolaan-perikanan">Pengelolaan Perikanan</Link></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
