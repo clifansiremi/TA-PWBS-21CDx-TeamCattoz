5424e204e113f96d3fd9710e36c4360232f195b1
riskaokta <riska_oktafia@teknokrat.ac.id>
2024-06-10 20:50:31 +0700

    membuat isi konservasi laut

--- frontend-admin/app/ksvlaut/page.tsx
+++ frontend-admin/app/ksvlaut/page.tsx
@@ -1,3 +1,14 @@
-export default function KonservasiLaut() {
-  return <div>Ini Halaman Menu Konservasi Laut</div>;
+import Link from 'next/link';
+import Navbar from '../components/Navbar';
+
+const KonservasiLaut: React.FC = () => {
+  return (
+    <div>
+      <h1>Konservasi Laut</h1>
+      <Navbar />
+      <p>Informasi mengenai konservasi laut.</p>
+    </div>
+  );
 }
+
+export default KonservasiLaut;
