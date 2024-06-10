48f548176ac064a2778b07279d828f33db5d6f47
riskaokta <riska_oktafia@teknokrat.ac.id>
2024-06-10 21:14:29 +0700

    ubah nama beranda

---
+++ frontend-admin/app/beranda/index.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+import Navbar from '../componen/navbar';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Beranda</h1>
+      <Navbar />
+      <p>Selamat datang di halaman beranda!</p>
+    </div>
+  );
+}
+
+
+export default Home;
