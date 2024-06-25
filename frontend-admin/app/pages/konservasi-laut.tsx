7909fe53231669d5f99e95caf98097ac84aacf09
riskaokta <riska_oktafia@teknokrat.ac.id>
2024-06-25 14:15:37 +0700

    menambahkan link tombol

--- frontend-admin/app/pages/konservasi-laut.tsx
+++ frontend-admin/app/pages/konservasi-laut.tsx
@@ -1,11 +1,18 @@
+// pages/konservasi-laut.tsx
+
 import Navbar from '../view/page';
+import ButtonLink from '../view/buttonlink';
 
 const KonservasiLaut: React.FC = () => {
   return (
     <div>
-      <h1>Konservasi Laut</h1>
       <Navbar />
-      <p>Informasi mengenai konservasi laut.</p>
+      <main>
+        <h1>Konservasi Laut</h1>
+        <ButtonLink href="/" text="Beranda" />
+        <ButtonLink href="/pengelolaan-perikanan" text="Pengelolaan Perikanan" />
+        <ButtonLink href="/about-me" text="About Me" />
+      </main>
     </div>
   );
 };
