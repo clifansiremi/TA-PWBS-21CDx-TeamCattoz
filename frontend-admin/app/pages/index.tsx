fb43029bf592761883ec5ca2135f9801ec9504cf
riskaokta <riska_oktafia@teknokrat.ac.id>
2024-06-25 14:15:22 +0700

    menambahkan link tombol

--- frontend-admin/app/pages/index.tsx
+++ frontend-admin/app/pages/index.tsx
@@ -1,15 +1,20 @@
-import Link from 'next/link';
+// pages/index.tsx
+
 import Navbar from '../view/page';
+import ButtonLink from '../view/buttonlink';
 
 const Home: React.FC = () => {
   return (
     <div>
-      <h1>Beranda</h1>
       <Navbar />
-      <p>Selamat datang di halaman beranda!</p>
+      <main>
+        <h1>Selamat Datang di Beranda</h1>
+        <ButtonLink href="/konservasi-laut" text="Konservasi Laut" />
+        <ButtonLink href="/pengelolaan-perikanan" text="Pengelolaan Perikanan" />
+        <ButtonLink href="/about-me" text="About Me" />
+      </main>
     </div>
   );
-}
-
+};
 
 export default Home;
