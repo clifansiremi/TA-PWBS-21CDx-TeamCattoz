a41cca40caaf56e5be784b0baaa1d4e087779b4e
riskaokta <riska_oktafia@teknokrat.ac.id>
2024-06-25 13:46:19 +0700

    mengubah link halaman

---
+++ frontend-admin/app/pages/konservasi-laut.tsx
@@ -0,0 +1,13 @@
+import Navbar from '../view/page';
+
+const KonservasiLaut: React.FC = () => {
+  return (
+    <div>
+      <h1>Konservasi Laut</h1>
+      <Navbar />
+      <p>Informasi mengenai konservasi laut.</p>
+    </div>
+  );
+};
+
+export default KonservasiLaut;
