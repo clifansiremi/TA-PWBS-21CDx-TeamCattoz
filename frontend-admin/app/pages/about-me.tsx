1eb67b2623c9812e8de700908a6dc92e50c5e7c9
riskaokta <riska_oktafia@teknokrat.ac.id>
2024-06-26 15:09:08 +0700

    menambahkan konten

--- frontend-admin/app/pages/about-me.tsx
+++ frontend-admin/app/pages/about-me.tsx
@@ -7,8 +7,11 @@ const AboutMe: React.FC = () => {
     <div>
       <Navbar />
       <main className={styles.main}>
+      <h1>About Me</h1>
+        <div className={styles.profile}>
+          <p>Halo! Kami dari Cattoz Team, kumpulan dari 3 orang yang menjadi seam untuk membuat website tentang kelautan, dalam website kami terdapat halaman konservasi laut dan juga perikanan, selamat menelusuri EDUKASEA.</p>
+        </div>
         
-
         <ButtonLink href="/" text="Beranda" />
         <ButtonLink href="/konservasi-laut" text="Konservasi Laut" />
         <ButtonLink href="/pengelolaan-perikanan" text="Pengelolaan Perikanan" />
