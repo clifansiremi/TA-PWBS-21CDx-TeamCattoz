be06d87e540c66d2146d0e22453bd99e5e16a2b8
riskaokta <riska_oktafia@teknokrat.ac.id>
2024-06-26 15:27:16 +0700

    menambahkan import statement dan state hook

--- frontend-admin/app/pages/index.tsx
+++ frontend-admin/app/pages/index.tsx
@@ -2,8 +2,14 @@
 
 import Navbar from '../view/page';
 import ButtonLink from '../view/buttonlink';
+import styles from './home.module.css';
+import Image from 'next/image';
+import { useState, useEffect } from 'react';
 
 const Home: React.FC = () => {
+  const images = ['/images/slide1.jpg', '/images/slide2.jpg', '/images/slide3.jpg', '/images/slide4.jpg', '/images/slide5.jpg'];
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
   return (
     <div>
       <Navbar />
