c9de64feb66dd9a5b2c8daa3c308ae598f15e830
riskaokta <riska_oktafia@teknokrat.ac.id>
2024-06-26 15:08:10 +0700

    menambahkan import statement

--- frontend-admin/app/pages/about-me.tsx
+++ frontend-admin/app/pages/about-me.tsx
@@ -1,20 +1,20 @@
-import Link from 'next/link';
-import Navbar from '../view/page';
 import ButtonLink from '../view/buttonlink';
+import Navbar from '../view/page';
+import styles from './aboutme.module.css';
 
 const AboutMe: React.FC = () => {
   return (
     <div>
       <Navbar />
-      <main>
-        <h1>About Me</h1>
+      <main className={styles.main}>
+        
+
         <ButtonLink href="/" text="Beranda" />
         <ButtonLink href="/konservasi-laut" text="Konservasi Laut" />
         <ButtonLink href="/pengelolaan-perikanan" text="Pengelolaan Perikanan" />
       </main>
     </div>
   );
-}
-
+};
 
 export default AboutMe;
