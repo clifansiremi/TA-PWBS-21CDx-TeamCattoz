92da19cf0b0bec7b657e5c1aa8a09fc02d6c2251
clifansiremi <sousdetoiles@gmail.com>
2024-06-29 21:47:46 +0700

    master: merubah field table pada database

---
+++ app/Http/Controllers/KelolaIkanController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\KelolaIkan;
+use Illuminate\Http\Request;
+
+class KelolaIkanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(KelolaIkan $kelolaIkan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(KelolaIkan $kelolaIkan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, KelolaIkan $kelolaIkan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(KelolaIkan $kelolaIkan)
+    {
+        //
+    }
+}
---
+++ database/migrations/2024_06_09_044907_create_ksv_lauts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ksv_laut', function (Blueprint $table) {
+            $table->id();
+
+            // field nama konten
+            $table->string('nama_konten', 100);
+
+            // field foto konten
+            $table->string('image');
+
+            // field deskripsi konten
+            $table->text('deskripsi_konten');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ksv_laut');
+    }
+};
